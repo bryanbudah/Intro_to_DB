909edb284944a5c4589d9a6cd7a5144389627c28
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-10-17 12:46:26 -0800

    coomit

--- task_2.sql
+++ task_2.sql
@@ -3,7 +3,7 @@ USE alx_book_store;
 
 -- Create AUTHORS table
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT AUTO_INCREMENT PRIMARY KEY
     author_name VARCHAR(215) NOT NULL
 );
 
